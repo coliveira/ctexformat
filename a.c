3f4d5fbf0583b86c3383f8bc4b04faa1f76c8d1d
Carlos Oliveira <coliveira@gmail.com>
2011-04-28 18:42:11 -0400

    print spaces

--- a.c
+++ a.c
@@ -31,13 +31,13 @@ char *keywords[] = {
 
 static int operatorId = 0;
 
+// return 1 for blank, 2 for newlines
 int isblank(int c) 
 {
    if (c == ' ' || c == '\t')
       return 1;
    if (c == '\n' || c == '\r') {
-      printf("\\\\%c", c);
-      return 1;
+      return 2;
    }
    return 0;
 }
@@ -67,14 +67,32 @@ int isTwoOperator()
    return 0;
 }
 
+int isidchar(char c, int afterfirst)
+{
+   int normalchar = ('a' <= c && c <= 'z') || ('A'<=c && c<='Z') || c=='_';
+   return normalchar || (afterfirst && (('0' <= c && c <= '9') || c=='.'));
+}
+
 int nexttoken(FILE *f) 
 {
    int c, i;
    int pos = 0;
    int ret = 0;
-   while ((c = fgetc(f)) != EOF && isblank(c)) 
-      ; /* advance over blanks */
+   int isid = 0;
+   int blanktype;
+   while ((c = fgetc(f)) != EOF && (blanktype = isblank(c))) {
+      if (blanktype == 2) printf("\\\\\n\\rule{0cm}{0cm}");
+      else printf("~");
+   }
    while (c != EOF &&  !isblank(c) && pos < MAX_TOKEN_SIZE) {
+      if (pos == 0 && isidchar(c, pos)) {
+         isid = 1;
+      } else if (isid && pos > 0 && !isidchar(c, pos)) {
+         /* cannot be part of a token */
+         ungetc(c, f);
+/*       printf("I am ungetting %c for pos %d\n", c, pos); */
+         break;
+      }
       token[pos++] = c;
       if (pos == 2) {
          if (isTwoOperator()) {
@@ -88,12 +106,13 @@ int nexttoken(FILE *f)
             break;
          }
       }
-      if (c == '\\' || c == '_' || c == '#' || c == '&') {
+      if (c == '\\' || c == '_' || (c == '#' && pos>1) ||  c == '&') {
          token[pos-1] = '\\';
          token[pos++] = c;
       }
       c = fgetc(f);
    }
+   if (isblank(c)) ungetc(c, f); // lets deal with this the next time
    token[pos] = '\0';
    if (pos == 1 && isOneOperator()) return IS_OPERATOR;
    if (ret) return ret;
@@ -123,23 +142,23 @@ int main(int argc, char **argv) {
    printf("\\documentclass{article}\\input{pre}\\begin{document}\n");
    while (!feof(f) && (ret = nexttoken(f)) ) {
       if (ret == IS_OPERATOR) {
-         char op[8];
+         char op[16];
          opid = operatorId;
          switch (opid) {
             case 0: strcpy(op, "\\{"); break;
             case 1: strcpy(op, "\\}"); break;
-            case 2: strcpy(op, "\\#"); break;
+            case 2: strcpy(op, "{\\tt \\#}"); break;
             case 3: strcpy(op, "$<$"); break;
             case 4: strcpy(op, "$>$"); break;
             case ASIZE(oneOperat): printf("{\\tt "); strcpy(op, "/*"); break;
             case ASIZE(oneOperat) + 1: printf("}"); strcpy(op, "*/"); break;
             default: strcpy(op, token);
          }
-         printf("{\\it %s}", op);
+         printf("{\\tt %s}", op);
       } else if (ret == IS_RESERVED) {
          printf("{\\bf %s}", token);
       } else {
-         printf("%s ", token);
+         printf("%s", token);
       }
    }
    printf("\\end{document}\n");

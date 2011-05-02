612210a242f459b2ce2f2f49e056de330eea1ca7
Carlos Oliveira <coliveira@gmail.com>
2011-05-02 08:33:40 -0400

    recognize strings

--- a.c
+++ a.c
@@ -15,6 +15,7 @@ enum TokenType {
    IS_ID,
    IS_RESERVED,
    IS_OPERATOR,
+   IS_STRING,
 };
 
 /* change this if adding single character operators */
@@ -69,59 +70,20 @@ int isidchar(char c, int afterfirst)
    return normalchar || (afterfirst && (('0' <= c && c <= '9') || c=='.'));
 }
 
-int nexttoken(FILE *f) 
+
+void texformat(int maxpos)
 {
-   int c, i, n;
-   int pos = 0;
-   int ret = 0;
-   int isid = 0;
-   int blanktype;
+   int c, i, n, pos;
 
-   /* write blanks */
-   while ((c = fgetc(f)) != EOF && (blanktype = isblank(c))) {
-      if (blanktype == 2) printf("\\\\\n\\rule{0cm}{0cm}");
-      else printf("~");
-   }
+   for (i=0; i<maxpos; ++i) tokencp[i] = token[i]; // make a copy of what we have
 
-   /* read a token */
-   while (c != EOF &&  !isblank(c) && pos < MAX_TOKEN_SIZE) {
-
-      /* is an id? then only [_A-Za-z] allowed */
-      if (pos == 0 && isidchar(c, pos)) {
-         isid = 1;
-      } else if (isid && pos > 0 && !isidchar(c, pos)) {
-         ungetc(c, f);
-         break;
-      }
-
-      token[pos++] = c;
-
-      /* is that an operator? return the appropriate code */
-      if (pos == 2 && isoperator()) {
-         ret = IS_OPERATOR;
-         if (operatorId < MAX_ONECHAR_OPERATOR_POS) {
-            ungetc(c, f);
-            pos--;
-         }
-         break;
-      }
-      c = fgetc(f);
-   }
-   if (isblank(c)) ungetc(c, f); /* lets deal with this the next time */
-   if (pos == 1) {
-      token[pos] = '\0';
-      if (isoperator()) return IS_OPERATOR; /* maybe returned before testing...*/
-   }
-
-   for (i=0; i<pos; ++i) tokencp[i] = token[i]; // make a copy of what we have
-
-   n = pos;
+   n = maxpos;
    pos = 0;
-   for (i=0; i<n; ++i) {
+   for (i=0; i<n && pos < MAX_TOKEN_SIZE; ++i) {
       c = tokencp[i];
       token[pos++] = c;
       /* convert into valid TeX characters */
-      if (c=='$' || c=='{' || c=='}' || c=='%' || c=='_' || (c=='#' && pos>1) || c=='&') {
+      if (c=='$' || c=='{' || c=='}' || c=='%' || c=='_' || c=='#' || c=='&') {
          if (pos + 2 > MAX_TOKEN_SIZE) FATAL("error: token is too long");
          token[pos-1] = '\\';
          token[pos++] = c;
@@ -146,7 +108,85 @@ int nexttoken(FILE *f)
          pos += 2;
       }
    }
+   if (pos >= MAX_TOKEN_SIZE) FATAL("error: token is too long");
    token[pos] = '\0';
+}
+
+int readstringlit(FILE *f)
+{
+   int pos = 0;
+   int c = fgetc(f);
+   while (c != EOF && c != '"' && pos < MAX_TOKEN_SIZE) {
+      if (c == '\\') {
+         c = fgetc(f);
+         if (c == '"') {
+            token[pos++] = '\\';
+         } else {
+            ungetc(c, f);
+            c = '\\';
+         }
+      }
+      token[pos++] = c;
+      c = fgetc(f);
+   }
+   if (pos >= MAX_TOKEN_SIZE) 
+      FATAL("error: literal string is too long");
+   if (c != '"') 
+      FATAL("error: invalid string literal");
+   texformat(pos);
+   return IS_STRING;
+}
+
+int nexttoken(FILE *f) 
+{
+   int c, i;
+   int pos = 0;
+   int ret = 0;
+   int isid = 0;
+   int blanktype;
+
+   /* write blanks */
+   while ((c = fgetc(f)) != EOF && (blanktype = isblank(c))) {
+      if (blanktype == 2) printf("\\\\\n\\rule{0cm}{0cm}");
+      else printf("~");
+   }
+
+   /* read a token */
+   while (c != EOF &&  !isblank(c) && pos < MAX_TOKEN_SIZE) {
+
+      /* is an id? then only [_A-Za-z] allowed */
+      if (pos == 0 && isidchar(c, pos)) {
+         isid = 1;
+      } else if (isid && pos > 0 && !isidchar(c, pos)) {
+         ungetc(c, f);
+         break;
+      } 
+
+      if (pos == 0 && c == '"') {
+         return readstringlit(f);
+      } 
+
+      token[pos++] = c;
+
+      /* is that an operator? return the appropriate code */
+      if (pos == 2 && isoperator()) {
+         ret = IS_OPERATOR;
+         if (operatorId < MAX_ONECHAR_OPERATOR_POS) {
+            ungetc(c, f);
+            pos--;
+         }
+         break;
+      }
+      c = fgetc(f);
+   }
+   if (isblank(c)) ungetc(c, f); /* lets deal with this the next time */
+   if (pos == 1) {
+      token[pos] = '\0';
+      if (isoperator()) return IS_OPERATOR; /* maybe returned before testing...*/
+   }
+
+   texformat(pos);
+
    if (ret) return ret;
 
    /* check for reserved keywords */
@@ -155,6 +195,8 @@ int nexttoken(FILE *f)
          return IS_RESERVED;
       }
    }
+
+   /* else it is just a common identifier */
    return IS_ID;
 }
 
@@ -190,9 +232,11 @@ int main(int argc, char **argv) {
          printf("{\\tt %s}", op);
       } else if (ret == IS_RESERVED) {
          printf("{\\bf %s}", token);
+      } else if (ret == IS_STRING) {
+         printf("``%s''", token);
       } else {
          printf("%s", token);
-      }
+      } 
    }
    printf("\\end{document}\n");
    return 0;

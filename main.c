02f0d76f3157676005eb9e8b95f7d7b114471f65
Carlos Oliveira <coliveira@gmail.com>
2011-05-04 08:50:35 -0400

    move comment code to its own function

--- main.c
+++ main.c
@@ -203,6 +203,40 @@ int readnumber(FILE *f)
    return type;
 }
 
+int readcomment(FILE *f)
+{
+   int blanktype;
+   int c = fgetc(f);
+   if (c == '/') { // this is a line comment
+      printf("{\\tt//");
+      while ((c = fgetc(f)) != EOF && c != '\n') {
+         // just print the comment
+         token[0] = c; texformat(1); printf("%s", token);
+      }
+      printf("}");
+   } else if (c == '*') {
+      /* There is a problem with alignment of first commented
+       * line with the next. The reason is that before the comment,
+       * blank characters "~" have the width of normal text. In the
+       * comment, they have the width of \tt text. */
+      printf("{\\tt/*");
+      while ((c = fgetc(f)) != EOF) {
+         if (c == '*') {
+            c = fgetc(f);
+            if (c == '/') break;  /* end of comment */
+            else { ungetc(c, f); c = '*'; }
+         }
+         if ((blanktype = isblank(c)) == 2) printf("\\\\\n\\rule{0cm}{0cm}");
+         else if (blanktype) printf("~");
+         else { token[0] = c; texformat(1); printf("%s", token); }
+      }
+      printf("*/}"); c = fgetc(f);
+   } else {
+      ungetc(c, f); c = '/';
+   }
+   return c;
+}
+
 int nexttoken(FILE *f) 
 {
    int pos = 0;
@@ -218,36 +252,8 @@ int nexttoken(FILE *f)
    }
 
    if (c == '/') {
-      c = fgetc(f);
-      if (c == '/') { // this is a line comment
-         printf("{\\tt//");
-         while ((c = fgetc(f)) != EOF && c != '\n') {
-            // just print the comment
-            token[0] = c; texformat(1); printf("%s", token);
-         }
-         printf("}");
-      } else if (c == '*') {
-         /* There is a problem with alignment of first commented
-          * line with the next. The reason is that before the comment,
-          * blank characters "~" have the width of normal text. In the
-          * comment, they have the width of \tt text. */
-         printf("{\\tt/*");
-         while ((c = fgetc(f)) != EOF) {
-            if (c == '*') {
-               c = fgetc(f);
-               if (c == '/') break;  /* end of comment */
-               else { ungetc(c, f); c = '*'; }
-            }
-            if ((blanktype = isblank(c)) == 2) printf("\\\\\n\\rule{0cm}{0cm}");
-            else if (blanktype) printf("~");
-            else { token[0] = c; texformat(1); printf("%s", token); }
-         }
-         printf("*/}"); c = fgetc(f);
-      } else {
-         ungetc(c, f); c = '/';
-      }
+      c = readcomment(f);
    }
-
    if (c == '"') {
       return readstringlit(f);
    } 
